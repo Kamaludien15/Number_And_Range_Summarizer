f56bf8ac14b04b444fbba7fce6c5d26610e1ae2f
Kamaludien <kamalsonday.158@gmail.com>
2023-04-17 15:51:05 +0200

    implemented summarizecollection method

--- src/main/java/NumberAndRangeSummarizer/NumberAndRangeSummarizer.java
+++ src/main/java/NumberAndRangeSummarizer/NumberAndRangeSummarizer.java
@@ -3,6 +3,7 @@ package NumberAndRangeSummarizer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NumberAndRangeSummarizer implements NumberRangeSummarizer {
     public Collection<Integer> collect(String input) {
@@ -27,6 +28,14 @@ public class NumberAndRangeSummarizer implements NumberRangeSummarizer {
     }
 
     public String summarizeCollection(Collection<Integer> input) {
-        return "Hello";
+        List<Integer> newList = new ArrayList<>(input);
+        List<String> items = newList.stream().map(n -> new Object() {final int number = n; final int group = newList.indexOf(n);})
+                .collect(Collectors.groupingBy(obj -> obj.group)).values().stream()
+                .map(g -> g.size() >=3
+                        ? g.get(0).number + "-" +g.get(g.size()-1).number
+                        : g.stream().map(obj -> String.valueOf(obj.number)).collect(Collectors.joining(", ")))
+                .collect(Collectors.toList());
+        String summarizedString = String.join(", ", items);
+        return summarizedString;
     }
 }
