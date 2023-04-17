351bfc033abeb5d148dda1e53a79ba4a808c4a76
Kamaludien <kamalsonday.158@gmail.com>
2023-04-17 15:55:59 +0200

    implemented summarizecollection method

--- src/main/java/NumberAndRangeSummarizer/NumberAndRangeSummarizer.java
+++ src/main/java/NumberAndRangeSummarizer/NumberAndRangeSummarizer.java
@@ -2,8 +2,8 @@ package NumberAndRangeSummarizer;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class NumberAndRangeSummarizer implements NumberRangeSummarizer {
     public Collection<Integer> collect(String input) {
@@ -28,14 +28,47 @@ public class NumberAndRangeSummarizer implements NumberRangeSummarizer {
     }
 
     public String summarizeCollection(Collection<Integer> input) {
-        List<Integer> newList = new ArrayList<>(input);
-        List<String> items = newList.stream().map(n -> new Object() {final int number = n; final int group = newList.indexOf(n);})
-                .collect(Collectors.groupingBy(obj -> obj.group)).values().stream()
-                .map(g -> g.size() >=3
-                        ? g.get(0).number + "-" +g.get(g.size()-1).number
-                        : g.stream().map(obj -> String.valueOf(obj.number)).collect(Collectors.joining(", ")))
-                .collect(Collectors.toList());
-        String summarizedString = String.join(", ", items);
-        return summarizedString;
+//        List<Integer> newList = new ArrayList<>(input);
+//        List<String> items = newList.stream().map(n -> new Object() {final int number = n; final int group = newList.indexOf(n);})
+//                .collect(Collectors.groupingBy(obj -> obj.group)).values().stream()
+//                .map(g -> g.size() >=3
+//                        ? g.get(0).number + "-" +g.get(g.size()-1).number
+//                        : g.stream().map(obj -> String.valueOf(obj.number)).collect(Collectors.joining(", ")))
+//                .collect(Collectors.toList());
+//        String summarizedString = String.join(", ", items);
+//
+        if (input == null || input.isEmpty()) {
+            return "";
+        }
+
+        List<Integer> sortedList = new ArrayList<>(input);
+        Collections.sort(sortedList);
+
+        List<String> summaryList = new ArrayList<>();
+        int start = sortedList.get(0);
+        int end = start;
+
+        for (int i = 1; i < sortedList.size(); i++) {
+            int current = sortedList.get(i);
+            if (current == end + 1) {
+                end = current;
+            } else {
+                if (start == end) {
+                    summaryList.add(Integer.toString(start));
+                } else {
+                    summaryList.add(start + "-" + end);
+                }
+                start = end = current;
+            }
+        }
+
+        if (start == end) {
+            summaryList.add(Integer.toString(start));
+        } else {
+            summaryList.add(start + "-" + end);
+        }
+
+        return String.join(", ", summaryList);
     }
+
 }
